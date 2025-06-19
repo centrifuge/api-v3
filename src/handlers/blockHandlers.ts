1d8d699519755e494cec74fa5c2a53f418234e44
Filippo Fontana <filippo@embrio.tech>
2025-06-19 14:59:10 +0200

    feat: enable token snapshots

--- src/handlers/blockHandlers.ts
+++ src/handlers/blockHandlers.ts
@@ -1,24 +1,37 @@
 import { ponder } from "ponder:registry";
 import { logEvent } from "../helpers/logger";
 import { Timekeeper } from "../helpers/timekeeper";
-import { PoolService } from "../services";
+import { BlockchainService, PoolService, TokenService } from "../services";
 import { PoolSnapshot } from "ponder:schema";
 import { snapshotter } from "../helpers/snapshotter";
 import { currentChains } from "../../ponder.config";
+import { TokenSnapshot } from "ponder:schema";
 
 const timekeeper = Timekeeper.start()
 
 async function processBlock(args: Parameters<Parameters<typeof ponder.on>[1]>[0]) {
   const chainName  = args.context.chain.name
-  const chainId = args.context.chain.id
   const { event, context } = args
   const newPeriod = await timekeeper.processBlock(context, event)
   if (!newPeriod) return
   logEvent(event, `${chainName}:newPeriod`)
+  const chainId = args.context.chain.id
+  if(typeof chainId !== "number") throw new Error("Chain ID is required")
+  const blockchain = await BlockchainService.get(context, { id: chainId.toString() })
+  const { centrifugeId } = blockchain.read()
   
   const pools = await PoolService.query(context, {
     isActive: true,
+    centrifugeId
   }) as PoolService[];
+
+  const tokens = await TokenService.query(context, {
+    isActive: true,
+    centrifugeId
+  }) as TokenService[];
+
+  await snapshotter(context, event, pools, PoolSnapshot)
+  await snapshotter(context, event, tokens, TokenSnapshot)
 }
 
 currentChains.forEach(chain => {
