3653c52cea6fbdc6f8db80b3c6bc120d072ed0ba
Filippo Fontana <filippo@embrio.tech>
2025-06-24 15:06:31 +0200

    feat: track escrows

---
+++ src/handlers/poolEscrowFactoryHandlers.ts
@@ -0,0 +1,22 @@
+
+
+import { ponder } from "ponder:registry";
+import { logEvent } from "../helpers/logger";
+import { BlockchainService } from "../services/BlockchainService";
+import { EscrowService } from "../services/EscrowService";
+
+ponder.on("PoolEscrowFactory:DeployPoolEscrow", async ({ event, context }) => {
+  logEvent(event, "PoolEscrowFactory:DeployPoolEscrow");
+  const chainId = context.chain.id
+  if (typeof chainId !== 'number') throw new Error('Chain ID is required')
+  const { poolId, escrow: escrowAddress } = event.args;
+
+  const blockchain = await BlockchainService.get(context, { id: chainId.toString() }) as BlockchainService
+  const { centrifugeId } = blockchain.read()
+
+  const escrow = (await EscrowService.init(context, {
+    address: escrowAddress,
+    poolId,
+    centrifugeId,
+  })) as EscrowService;
+});
