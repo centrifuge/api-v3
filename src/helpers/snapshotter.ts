0a6d36b838173c4b758b834a2d62189787c8e0d7
Filippo Fontana <filippo@embrio.tech>
2025-06-27 14:18:28 +0200

    40-Filter-snapshots-by-triggering-events (#48)
    * feat: include triggering event in snapshots
    implements #40
    
    * feat: include triggering event in snapshots
    implements #40

--- src/helpers/snapshotter.ts
+++ src/helpers/snapshotter.ts
@@ -1,14 +1,15 @@
 import { Service } from "../services/Service";
+import { ponder } from "ponder:registry";
 import type { Context, Event } from "ponder:registry";
 import { PgTableWithColumns } from "drizzle-orm/pg-core";
 
-
+type TriggerEvent = Parameters<typeof ponder.on>[0] | `${string}:NewPeriod`
 
 export async function snapshotter<
   S extends Service<T>,
   T extends PgTableWithColumns<any>,
   ST extends PgTableWithColumns<any>
->(context: Context, event: Event, entities: S[], snapshotTable: ST ){
+>(context: Context, event: Event, trigger: TriggerEvent, entities: S[], snapshotTable: ST ){
   if (entities.length === 0) {
     console.log("No entities to snapshot")
     return
@@ -16,7 +17,7 @@ export async function snapshotter<
   for (const entity of entities) {
     const data = entity.read();
     console.log(`snapshotting ${data['id']}`)
-    const snapshotData = { ...data, timestamp: new Date(Number(event.block.timestamp) * 1000), blockNumber: Number(event.block.number) };
+    const snapshotData = { ...data, timestamp: new Date(Number(event.block.timestamp) * 1000), blockNumber: Number(event.block.number), trigger };
     await context.db.sql.insert(snapshotTable).values(snapshotData);
   }
 }
