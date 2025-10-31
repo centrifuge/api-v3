e7b9d7ba5d581105d0f83cd37c71ed341467013b
Filippo Fontana <filippo@embrio.tech>
2025-10-31 17:51:59 +0100

    chore: use existing default services in readOnly mode

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import { db } from "ponder:api";
 import schema from "ponder:schema";
 import { Hono } from "hono";
 import { graphql } from "ponder";
-import { TokenService } from "../services/TokenService";
+import { TokenInstanceService } from "../services";
 
 const app = new Hono();
 
@@ -12,19 +12,21 @@ app.use("/graphql", graphql({ db, schema }));
 app.get("/tokens/:address/total-issuance", async (c) => {
   const address = c.req.param("address") as `0x${string}`;
 
-  const token = await TokenService.getTokenByInstanceAddress(db, address);
-
-  if (!token) return c.json({ error: "Token not found" }, 404);
-  return c.json({ result: String(token.totalIssuance) });
+  const tokenInstance = await TokenInstanceService.get(null, { address });
+  if (!tokenInstance) return c.json({ error: "Token not found" }, 404);
+  const { totalIssuance } = tokenInstance.read();
+  if(totalIssuance === null) return c.json({ error: "Total issuance not set" }, 404);
+  return c.json({ result: totalIssuance.toString() });
 });
 
 app.get("/tokens/:address/price", async (c) => {
   const address = c.req.param("address") as `0x${string}`;
 
-  const token = await TokenService.getTokenByInstanceAddress(db, address);
-
+  const token = await TokenInstanceService.get(null, { address });
   if (!token) return c.json({ error: "Token not found" }, 404);
-  return c.json({ result: String(token.tokenPrice) });
+  const { tokenPrice } = token.read();
+  if(tokenPrice === null) return c.json({ error: "Token price not set" }, 404);
+  return c.json({ result: tokenPrice.toString() });
 });
 
 export default app;
