777bcf9eb6435acbafc4d8d1f1a4f5372ace8497
Filippo Fontana <filippo@embrio.tech>
2025-10-31 18:57:30 +0100

    chore: use tokens for price

--- src/api/index.ts
+++ src/api/index.ts
@@ -29,8 +29,13 @@ app.get("/tokens/:address/total-issuance", async (c) => {
 app.get("/tokens/:address/price", async (c) => {
   const address = c.req.param("address") as `0x${string}`;
 
-  const token = await TokenInstanceService.get(context, { address });
+  const tokenInstance = await TokenInstanceService.get(context, { address });
+  if (!tokenInstance) return c.json({ error: "TokenInstance not found" }, 404);
+  const { tokenId } = tokenInstance.read();
+
+  const token = await TokenService.get(context, { id: tokenId });
   if (!token) return c.json({ error: "Token not found" }, 404);
+  
   const { tokenPrice } = token.read();
   if (tokenPrice === null) return c.json({ error: "Token price not set" }, 404);
   return c.json({ result: tokenPrice.toString() });
