355e7695cc0078dc79bbee3b5e8ee9305d8f9d77
Filippo Fontana <filippo@embrio.tech>
2025-11-04 16:43:14 +0100

    chore: default headers

--- src/api/index.ts
+++ src/api/index.ts
@@ -11,39 +11,61 @@ const context = { db };
 app.use("/", graphql({ db, schema }));
 app.use("/graphql", graphql({ db, schema }));
 
+const jsonDefaultHeaders = {
+  charset: "utf-8",
+  "Content-Type": "application/json",
+};
 app.get("/tokens/:address/total-issuance", async (c) => {
   const address = c.req.param("address") as `0x${string}`;
-
   const tokenInstance = await TokenInstanceService.get(context, { address });
   if (!tokenInstance)
-    return c.json({ error: "TokenInstance address not found" }, 404);
+    return c.json(
+      { error: "TokenInstance address not found" },
+      404,
+      jsonDefaultHeaders
+    );
   const { tokenId } = tokenInstance.read();
 
   const token = await TokenService.get(context, { id: tokenId });
-  if (!token) return c.json({ error: "Token not found" }, 404);
+  if (!token)
+    return c.json({ error: "Token not found" }, 404, jsonDefaultHeaders);
 
   const { totalIssuance, decimals } = token.read();
   if (totalIssuance === null)
-    return c.json({ error: "Total issuance not set" }, 404);
+    return c.json({ error: "Total issuance not set" }, 404, jsonDefaultHeaders);
   if (decimals === null)
-    return c.json({ error: "Token decimals not set" }, 404);
-  return c.json({ result: formatBigIntToDecimal(totalIssuance, decimals) }, 200);
+    return c.json({ error: "Token decimals not set" }, 404, jsonDefaultHeaders);
+  return c.json(
+    { result: formatBigIntToDecimal(totalIssuance, decimals) },
+    200,
+    jsonDefaultHeaders
+  );
 });
 
 app.get("/tokens/:address/price", async (c) => {
   const address = c.req.param("address") as `0x${string}`;
 
   const tokenInstance = await TokenInstanceService.get(context, { address });
-  if (!tokenInstance) return c.json({ error: "TokenInstance not found" }, 404);
+  if (!tokenInstance)
+    return c.json(
+      { error: "TokenInstance not found" },
+      404,
+      jsonDefaultHeaders
+    );
   const { tokenId } = tokenInstance.read();
 
   const token = await TokenService.get(context, { id: tokenId });
-  if (!token) return c.json({ error: "Token not found" }, 404);
+  if (!token)
+    return c.json({ error: "Token not found" }, 404, jsonDefaultHeaders);
 
   const { tokenPrice } = token.read();
   if (tokenPrice === null) return c.json({ error: "Token price not set" }, 404);
 
-  return c.json({ result: formatBigIntToDecimal(tokenPrice) }, 200);
+  return c.json(
+    { result: formatBigIntToDecimal(tokenPrice) },
+    200,
+    jsonDefaultHeaders
+  );
 });
 
 export default app;
