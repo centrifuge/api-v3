7af4f2bc08e37e6f0a531eb6306b93e6689fa248
Filippo Fontana <filippo@embrio.tech>
2026-07-30 13:05:53 +0200

    feat: ipfs gateway expose on env

--- src/helpers/ipfs.ts
+++ src/helpers/ipfs.ts
@@ -1,10 +1,20 @@
 import fetch from "node-fetch";
 import { serviceError } from "./logger";
 
-const IPFS_GATEWAY = "https://centrifuge.mypinata.cloud/ipfs/";
+const DEFAULT_IPFS_GATEWAY = "https://ipfs.centrifuge.io/ipfs/";
+const IPFS_GATEWAY = process.env.IPFS_GATEWAY
+  ? process.env.IPFS_GATEWAY.endsWith("/")
+    ? process.env.IPFS_GATEWAY
+    : `${process.env.IPFS_GATEWAY}/`
+  : DEFAULT_IPFS_GATEWAY;
 
 /**
- * Fetches and parses JSON data from IPFS using the configured gateway
+ * Fetches and parses JSON data from IPFS using the configured gateway.
+ *
+ * Gateway is `https://ipfs.centrifuge.io/ipfs/` by default (matches the registry
+ * fetcher) and can be overridden via the `IPFS_GATEWAY` env var. A trailing slash
+ * is enforced.
+ *
  * @param ipfsHash - The IPFS hash/CID to fetch. Can optionally include 'ipfs://' prefix
  * @returns {Promise<any>} The parsed JSON data from IPFS
  * @throws {Error} If the IPFS fetch fails or response is not OK
